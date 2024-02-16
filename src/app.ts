893328ed834fc584c04f0ee9224f545977a1c8d3
DillusDev001 <dillus.dev.001@gmail.com>
2024-02-16 16:51:24 -0400

    qa

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { MemoryDB, addKeyword, createBot, createFlow, createProvider } from "@bot-whatsapp/bot";
-import { BaileysProvider } from "@bot-whatsapp/provider-baileys";
+import { BaileysProvider, handleCtx } from "@bot-whatsapp/provider-baileys";
 
 const flowBienvenida = addKeyword('hola').addAnswer('Hola, soy Diego tu asistente virtual.');
 const flowPregunta = addKeyword('como estas?').addAnswer('Bien Gracias');
+
+
 
 /**
  * 
@@ -11,6 +13,13 @@ const flowPregunta = addKeyword('como estas?').addAnswer('Bien Gracias');
 const main = async () => {
 
     const provider = createProvider(BaileysProvider);
+
+    provider.initHttpServer(3002);
+
+    provider.http?.server.post('/send-message', handleCtx(async(bot, req, res) => {
+        await bot.sendMessage('','', {})
+        res.end('Server');
+    }))
 
     await createBot({
         flow:createFlow([flowBienvenida, flowPregunta]),
